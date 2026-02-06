029e2e9646d5780c960e5f9e7419aa6d3675ff2a
LeraShil <valeriya2007@icloud.com>
2026-02-06 22:32:26 +0300

    feat(user): define UserDTO

---
+++ src/dto/user_dto.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <userver/formats/serialize/to.hpp>
+#include <userver/formats/parse/to.hpp>
+#include <userver/formats/json/value.hpp>
+
+namespace pawspective::dto {
+
+struct UserDTO {
+    std::int64_t id;
+    std::string email;
+    std::string first_name;
+    std::string last_name;
+    std::optional<std::int64_t> organization_id;
+};
+
+userver::formats::json::Value
+serialize(const UserDTO &data, userver::formats::serialize::To<userver::formats::json::Value>);
+UserDTO
+parse(const userver::formats::json::Value &json, userver::formats::parse::To<UserDTO>);
+
+}  // namespace pawspective::dto
