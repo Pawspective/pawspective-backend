c50ab48efdd29c2729ddd50c4fc2961c3e374817
LeraShil <valeriya2007@icloud.com>
2026-02-12 19:50:42 +0000

    feat(user): define UserRepository interface

---
+++ src/repositories/include/user_repository.cpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <userver/storages/postgres/cluster.hpp>
+#include "../../models/include/user.hpp"
+
+namespace pawspective::repositories {
+
+class UserRepository {
+private:
+    userver::storages::postgres::ClusterPtr pg_cluster_{};
+
+public:
+    UserRepository(userver::storages::postgres::ClusterPtr pg_cluster)
+        : pg_cluster_(std::move(pg_cluster)) {
+    }
+
+    std::optional<models::User> get_by_id(std::int64_t id);
+    std::optional<models::User> get_by_email(const std::string &email);
+    models::User create_user(const models::User &user);
+    models::User update_user(std::int64_t id, const models::User &user_upd);
+    models::User link_organization(std::int64_t id, std::int64_t org_id);
+    models::User
+    set_password_hash(std::int64_t id, const std::string &password_hash);
+    bool delete_user(std::int64_t id);
+    bool check_can_create_shelter(std::int64_t id);
+    // std::optional<models::Organization>
+    // get_organization_by_user_id(std::int64_t id); // TODO: Uncomment after
+    // creation Organization
+};
+}  // namespace pawspective::repositories
