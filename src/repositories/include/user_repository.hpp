e81be3980e711e2b4080a1ddfb3c94c58812ebb6
LeraShil <valeriya2007@icloud.com>
2026-02-12 21:41:40 +0000

    refactor(user): add const

--- src/repositories/include/user_repository.hpp
+++ src/repositories/include/user_repository.hpp
@@ -14,14 +14,18 @@ private:
 public:
     explicit UserRepository(userver::storages::postgres::ClusterPtr pg_cluster);
 
-    std::optional<models::User> get_by_id(std::int64_t id);
-    std::optional<models::User> get_by_email(const std::string &email);
-    models::User create_user(const models::User &user);
-    models::User update_user(std::int64_t id, const models::User &user_upd);
-    models::User link_organization(std::int64_t id, std::int64_t org_id);
-    models::User
-    set_password_hash(std::int64_t id, const std::string &password_hash);
-    bool delete_user(std::int64_t id);
+    [[nodiscard]] std::optional<models::User> get_by_id(std::int64_t id) const;
+    [[nodiscard]] std::optional<models::User> get_by_email(
+        const std::string &email
+    ) const;
+    [[nodiscard]] models::User create_user(const models::User &user) const;
+    [[nodiscard]] models::User
+    update_user(std::int64_t id, const models::User &user_upd) const;
+    [[nodiscard]] models::User
+    link_organization(std::int64_t id, std::int64_t org_id) const;
+    [[nodiscard]] models::User
+    set_password_hash(std::int64_t id, const std::string &password_hash) const;
+    [[nodiscard]] bool delete_user(std::int64_t id) const;
     [[nodiscard]] bool check_can_create_shelter(std::int64_t id) const;
     // std::optional<models::Organization>
     // get_organization_by_user_id(std::int64_t id); // TODO: Uncomment after
