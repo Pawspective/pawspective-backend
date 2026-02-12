6e8a1035d761b0199b6860ece21040a3b2b72c39
LeraShil <valeriya2007@icloud.com>
2026-02-12 21:14:32 +0000

    refactor(user): change user_repository.hpp

--- src/repositories/include/user_repository.hpp
+++ src/repositories/include/user_repository.hpp
@@ -3,18 +3,26 @@
 #include <cstdint>
 #include <optional>
 #include <string>
+<<<<<<< HEAD
 #include <userver/storages/postgres/cluster.hpp>
+=======
+>>>>>>> df60ae5 (refactor(user): change user_repository.hpp)
 #include "../../models/include/user.hpp"
 
 namespace pawspective::repositories {
 
 class UserRepository {
 private:
+<<<<<<< HEAD
     userver::storages::postgres::ClusterPtr pg_cluster_;
+=======
+    userver::storages::postgres::ClusterPtr pg_cluster_{};
+>>>>>>> df60ae5 (refactor(user): change user_repository.hpp)
 
 public:
     explicit UserRepository(userver::storages::postgres::ClusterPtr pg_cluster);
 
+<<<<<<< HEAD
     [[nodiscard]] std::optional<models::User> get_by_id(std::int64_t id) const;
     [[nodiscard]] std::optional<models::User> get_by_email(
         const std::string &email
@@ -28,6 +36,16 @@ public:
     set_password_hash(std::int64_t id, const std::string &password_hash) const;
     bool delete_user(std::int64_t id) const;
     // NOLINTEND(modernize-use-nodiscard)
+=======
+    std::optional<models::User> get_by_id(std::int64_t id);
+    std::optional<models::User> get_by_email(const std::string &email);
+    models::User create_user(const models::User &user);
+    models::User update_user(std::int64_t id, const models::User &user_upd);
+    models::User link_organization(std::int64_t id, std::int64_t org_id);
+    models::User
+    set_password_hash(std::int64_t id, const std::string &password_hash);
+    bool delete_user(std::int64_t id);
+>>>>>>> df60ae5 (refactor(user): change user_repository.hpp)
     [[nodiscard]] bool check_can_create_shelter(std::int64_t id) const;
     // std::optional<models::Organization>
     // get_organization_by_user_id(std::int64_t id); // TODO: Uncomment after
