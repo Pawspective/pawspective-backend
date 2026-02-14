0998f136323c14bac41c7b975ca64230f2b33426
FlintWithBlackCrown <kbolokhov@gmail.com>
2026-02-14 22:27:18 +0000

    refactor: no keys hardcoded in static_config.yaml yet

---
+++ src/models/src/jwt_secrets.cpp
@@ -0,0 +1,16 @@
+#include "jwt_secrets.hpp"
+#include <string>
+#include <userver/formats/json.hpp>  // NOLINT [misc-include-cleaner]
+
+namespace pawspective::models {
+JwtSecrets
+// NOLINTNEXTLINE [readability-identifier-naming]
+Parse(const userver::formats::json::Value &value, userver::formats::parse::To<JwtSecrets>) {
+    return JwtSecrets{value};
+}
+
+JwtSecrets::JwtSecrets(const userver::formats::json::Value &value)
+    : secret_key(value["jwt-service"]["secret_key"].As<std::string>()) {
+}
+
+}  // namespace pawspective::models
