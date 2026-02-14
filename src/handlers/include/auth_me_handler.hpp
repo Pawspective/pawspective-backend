3b7aae4b293a675ef9e2a9e514c826eba558eb1a
LeraShil <valeriya2007@icloud.com>
2026-02-14 23:34:22 +0000

    wip: auth handler

---
+++ src/handlers/include/auth_me_handler.hpp
@@ -0,0 +1,47 @@
+#pragma once
+//TODO: change config?
+#include <userver/components/component.hpp>
+#include <userver/server/handlers/auth/auth_checker_base.hpp>
+#include <userver/server/handlers/auth/auth_checker_settings.hpp>
+#include <userver/server/handlers/http_handler_json_base.hpp>
+#include "../../services/include/jwt_service.hpp"
+
+namespace pawspective::handlers {
+class AuthMeChecker final
+    : public userver::server::handlers::auth::AuthCheckerBase {
+public:
+    using AuthCheckerResult = userver::server::handlers::auth::AuthCheckResult;
+    explicit AuthMeChecker(services::JwtService &jwt_service);
+
+    [[nodiscard]] AuthCheckerResult CheckAuth(
+        const userver::server::http::HttpRequest &request,
+        userver::server::request::RequestContext &request_context
+    ) const override;
+    [[nodiscard]] bool SupportsUserAuth() const noexcept override;
+
+private:
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-const-or-ref-data-members)
+    services::JwtService &jwt_service_;
+};
+
+class AuthMeHandler final
+    : public userver::server::handlers::HttpHandlerJsonBase {
+public:
+    constexpr static std::string_view kName = "auth-me-handler";
+    AuthMeHandler(
+        const userver::components::ComponentConfig &config,
+        const userver::components::ComponentContext &component_context,
+        bool is_monitor = false
+    );
+    userver::formats::json::Value HandleRequestJsonThrow(
+        const userver::server::http::HttpRequest &request,
+        const userver::formats::json::Value &request_json,
+        userver::server::request::RequestContext &context
+    ) const override;
+
+private:
+    services::JwtService &jwt_service_;
+    // services::UserService& user_service_; // uncomment when UserServise will be done
+};
+
+}  // namespace pawspective::handlers
---
+++ src/handlers/src/auth_me_handler.cpp
@@ -0,0 +1,77 @@
+#include "../include/auth_me_handler.hpp"
+#include <userver/components/component.hpp>
+#include <userver/http/common_headers.hpp>
+#include "../../services/include/jwt_service.hpp"
+
+namespace pawspective::handlers {
+
+AuthMeChecker::AuthMeChecker(services::JwtService &jwt_service)
+    : jwt_service_(jwt_service) {
+}
+
+AuthMeChecker::AuthCheckerResult AuthMeChecker::CheckAuth(
+    const userver::server::http::HttpRequest &request,
+    userver::server::request::RequestContext &request_context
+) const {
+    const auto &auth_header =
+        request.GetHeader(userver::http::headers::kAuthorization);
+    if (auth_header.empty()) {
+        return AuthCheckerResult{
+            AuthCheckerResult::Status::kTokenNotFound,
+            {},
+            "Empty Authorization header",
+            userver::server::handlers::HandlerErrorCode::kUnauthorized
+        };
+    }
+
+    const auto bearer_sep_pos = auth_header.find(' ');
+    if (bearer_sep_pos == std::string::npos ||
+        auth_header.substr(0, bearer_sep_pos) != "Bearer") {
+        return AuthCheckerResult{
+            AuthCheckerResult::Status::kTokenNotFound,
+            {},
+            "Invalid Authorization header format. Expected: Bearer <token>",
+            userver::server::handlers::HandlerErrorCode::kUnauthorized
+        };
+    }
+    std::string token = auth_header.substr(bearer_sep_pos + 1);
+    auto payload = jwt_service_.validate_access_token(token);
+    if (!payload) {
+        return AuthCheckerResult{
+            AuthCheckerResult::Status::kForbidden,
+            {},
+            "Invalid or expired access token",
+            userver::server::handlers::HandlerErrorCode::kUnauthorized
+        };
+    }
+
+    request_context.SetData("user_id", payload->user_id);
+    return {};
+}
+
+bool AuthMeChecker::SupportsUserAuth() const noexcept {
+    return true;
+}
+
+AuthMeHandler::AuthMeHandler(
+    const userver::components::ComponentConfig &config,
+    const userver::components::ComponentContext &component_context,
+    bool is_monitor
+)
+    : HttpHandlerJsonBase(config, component_context, is_monitor),
+      jwt_service_(component_context.FindComponent<services::JwtService>()) {
+}
+
+userver::formats::json::Value AuthMeHandler::HandleRequestJsonThrow(
+    const userver::server::http::HttpRequest &request,
+    const userver::formats::json::Value &,
+    userver::server::request::RequestContext &context
+) const {
+    const auto &user_id = context.GetData<std::string>("user_id");
+    userver::formats::json::ValueBuilder response;  // TODO: add UserDTO
+    response["id"] = user_id;
+    request.SetResponseStatus(userver::server::http::HttpStatus::kOk);
+    return response.ExtractValue();
+}
+
+}  // namespace pawspective::handlers
