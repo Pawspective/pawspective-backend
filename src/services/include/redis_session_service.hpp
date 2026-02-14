da19c5553f0d94188256e8b85a632e73f2012fe5
FlintWithBlackCrown <kbolokhov@gmail.com>
2026-02-14 12:52:43 +0000

    refactor: make format

--- src/services/include/redis_session_service.hpp
+++ src/services/include/redis_session_service.hpp
@@ -1,24 +1,27 @@
 #pragma once
 
-#include "jwt_service.hpp"
-#include "session_service.hpp"
 #include <optional>
 #include <string_view>
 #include <userver/storages/redis/client.hpp>
+#include "jwt_service.hpp"
+#include "session_service.hpp"
+
 namespace pawspective::services {
 
 class RedisSessionService final : public SessionService {
     RedisSessionService(
         userver::storages::redis::ClientPtr redis_client,
-        JwtService& jwt
+        JwtService &jwt
     );
 
     SessionBundle create_session(std::string_view user_id) override;
-    std::optional<TokenPayload> validate_session(std::string_view refresh_token) override;
+    std::optional<TokenPayload> validate_session(std::string_view refresh_token
+    ) override;
     void revoke_session(std::string_view refresh_token) override;
+
 private:
     userver::storages::redis::ClientPtr redis_;
-    JwtService& jwt_;
+    JwtService &jwt_;
 };
 
-}
+}  // namespace pawspective::services
--- src/services/src/redis_session_service.cpp
+++ src/services/src/redis_session_service.cpp
@@ -1,3 +1,4 @@
+#include "redis_session_service.hpp"
 #include <chrono>
 #include <optional>
 #include <string>
@@ -7,51 +8,50 @@
 #include <userver/storages/redis/client_fwd.hpp>
 #include <utility>
 #include "jwt_service.hpp"
-#include "redis_session_service.hpp"
 
 namespace pawspective::services {
 
-    RedisSessionService::RedisSessionService(
-        userver::storages::redis::ClientPtr redis_client,
-        JwtService& jwt
-    )
-        : redis_(std::move(redis_client)), jwt_(jwt) {
+RedisSessionService::RedisSessionService(
+    userver::storages::redis::ClientPtr redis_client,
+    JwtService &jwt
+)
+    : redis_(std::move(redis_client)), jwt_(jwt) {
+}
+
+SessionBundle RedisSessionService::create_session(std::string_view user_id) {
+    const auto access_token = jwt_.generate_access_token(user_id);
+    const auto refresh_token = jwt_.generate_refresh_token(user_id);
+
+    SessionBundle bundle{access_token, refresh_token};
+
+    auto hash = userver::crypto::hash::Sha256(bundle.refresh_token);
+
+    redis_
+        ->Setex(
+            "sess:" + hash, std::chrono::hours(24 * 7), std::string(user_id), {}
+        )
+        .Get();
+
+    return bundle;
+}
+
+std::optional<TokenPayload> RedisSessionService::validate_session(
+    std::string_view refresh_token
+) {
+    auto payload = jwt_.validate_refresh_token(refresh_token);
+    if (!payload) {
+        return std::nullopt;
     }
 
-    SessionBundle RedisSessionService::create_session(std::string_view user_id) {
-        const auto access_token = jwt_.generate_access_token(user_id);
-        const auto refresh_token = jwt_.generate_refresh_token(user_id);
+    auto hash = userver::crypto::hash::Sha256(refresh_token);
+    auto exists = redis_->Get("sess:" + hash, {}).Get();
 
-        SessionBundle bundle{access_token, refresh_token};
+    return exists ? payload : std::nullopt;
+}
 
-        auto hash = userver::crypto::hash::Sha256(bundle.refresh_token);
-
-        redis_
-            ->Setex(
-                "sess:" + hash, std::chrono::hours(24 * 7),
-                std::string(user_id), {}
-            )
-            .Get();
-
-        return bundle;
-    }
-
-    std::optional<TokenPayload> RedisSessionService::validate_session(std::string_view refresh_token) {
-        auto payload = jwt_.validate_refresh_token(refresh_token);
-        if (!payload) {
-            return std::nullopt;
-        }
-
-        auto hash = userver::crypto::hash::Sha256(refresh_token);
-        auto exists = redis_->Get("sess:" + hash, {}).Get();
-
-        return exists ? payload : std::nullopt;
-    }
-
-    void RedisSessionService::revoke_session(std::string_view refresh_token) {
-        auto hash = userver::crypto::hash::Sha256(refresh_token);
-        redis_->Del("sess:" + hash, {}).Get();
-    }
-
+void RedisSessionService::revoke_session(std::string_view refresh_token) {
+    auto hash = userver::crypto::hash::Sha256(refresh_token);
+    redis_->Del("sess:" + hash, {}).Get();
+}
 
 }  // namespace pawspective::services
