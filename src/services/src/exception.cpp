b73c934feed60ae7ed8dce9d570fc931152e8c5f
Egor Maximov <maksimov.egor07@gmail.com>
2026-02-24 01:37:33 +0300

    feat: add user service

--- src/services/src/exception.cpp
+++ src/services/src/exception.cpp
@@ -4,4 +4,9 @@
 
 namespace pawspective::services {
 UserAlreadyExistsException::UserAlreadyExistsException() : std::runtime_error("User already exists") {}
+
+UserNotFoundException::UserNotFoundException() : std::runtime_error("User not found") {}
+
+InvalidCredentialsException::InvalidCredentialsException() : std::runtime_error("Invalid credentials") {}
+
 }  // namespace pawspective::services
