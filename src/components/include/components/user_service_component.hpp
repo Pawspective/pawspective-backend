751af7c24d90bc3ab325fb9acabee81801777ddd
Egor Maximov <maksimov.egor07@gmail.com>
2026-02-27 15:08:23 +0300

    style: reformat code using clang-format

--- src/components/include/components/user_service_component.hpp
+++ src/components/include/components/user_service_component.hpp
@@ -4,16 +4,17 @@
 #include "services/user_service.hpp"
 
 namespace pawspective::components {
-    class UserServiceComponent : public userver::components::LoggableComponentBase {
-    public:
-        static constexpr std::string_view kName = "user-service";
-        explicit UserServiceComponent(
-            const userver::components::ComponentConfig& config,
-            const userver::components::ComponentContext& context
-        );
+class UserServiceComponent : public userver::components::LoggableComponentBase {
+public:
+    static constexpr std::string_view kName = "user-service";
+    explicit UserServiceComponent(
+        const userver::components::ComponentConfig& config,
+        const userver::components::ComponentContext& context
+    );
 
-        [[nodiscard]] const services::UserService& get_service() const;
-    private:
-        const services::UserService service_;
-    };
-} // namespace pawspective::components
+    [[nodiscard]] const services::UserService& get_service() const;
+
+private:
+    const services::UserService service_;
+};
+}  // namespace pawspective::components
--- src/components/src/user_service_component.cpp
+++ src/components/src/user_service_component.cpp
@@ -3,14 +3,13 @@
 #include "user_repository_component.hpp"
 
 namespace pawspective::components {
-    UserServiceComponent::UserServiceComponent(
-        const userver::components::ComponentConfig& config,
-        const userver::components::ComponentContext& context
-    ) : LoggableComponentBase(config, context),
-        service_(context.FindComponent<UserRepositoryComponent>().get_repository()) {}
+UserServiceComponent::UserServiceComponent(
+    const userver::components::ComponentConfig& config,
+    const userver::components::ComponentContext& context
+)
+    : LoggableComponentBase(config, context),
+      service_(context.FindComponent<UserRepositoryComponent>().get_repository()) {}
 
-    const services::UserService& UserServiceComponent::get_service() const {
-        return service_;
-    }
+const services::UserService& UserServiceComponent::get_service() const { return service_; }
 
-} // namespace pawspective::components
+}  // namespace pawspective::components
