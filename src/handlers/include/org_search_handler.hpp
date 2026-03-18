5215da8fa3e471520e6837f526f591bce8865eaa
FlintWithBlackCrown <92873054+Granzh@users.noreply.github.com>
2026-03-18 17:42:05 +0300

    feat: add handler and tests (#104)
    * feat: add handler and tests

---
+++ src/handlers/include/org_search_handler.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <userver/server/handlers/http_handler_json_base.hpp>
+#include "organization_service_component.hpp"
+
+namespace pawspective::handlers {
+
+class OrgSearchHandler final : public userver::server::handlers::HttpHandlerJsonBase {
+public:
+    constexpr static std::string_view kName = "handler-org-search";
+
+    OrgSearchHandler(
+        const userver::components::ComponentConfig& config,
+        const userver::components::ComponentContext& component_context,
+        bool is_monitor = false
+    );
+
+    userver::formats::json::Value HandleRequestJsonThrow(
+        const userver::server::http::HttpRequest& request,
+        const userver::formats::json::Value& request_json,
+        userver::server::request::RequestContext& context
+    ) const override;
+
+private:
+    const components::OrganizationServiceComponent& org_service_;
+};
+
+}  // namespace pawspective::handlers
---
+++ src/handlers/src/org_search_handler.cpp
@@ -0,0 +1,28 @@
+#include "org_search_handler.hpp"
+
+#include <userver/components/component_context.hpp>
+#include <userver/formats/json/value_builder.hpp>
+#include <userver/formats/serialize/common_containers.hpp>  // NOLINT
+#include "organization_service_component.hpp"
+
+namespace pawspective::handlers {
+
+OrgSearchHandler::OrgSearchHandler(
+    const userver::components::ComponentConfig& config,
+    const userver::components::ComponentContext& component_context,
+    bool is_monitor
+)
+    : HttpHandlerJsonBase(config, component_context, is_monitor),
+      org_service_(component_context.FindComponent<components::OrganizationServiceComponent>()) {}
+
+userver::formats::json::Value OrgSearchHandler::HandleRequestJsonThrow(
+    const userver::server::http::HttpRequest& request,
+    const userver::formats::json::Value& /*request_json*/,
+    userver::server::request::RequestContext& /*context*/
+) const {
+    const auto& name = request.GetArg("name");
+    const auto orgs = org_service_.get_service().FindByNameContaining(name);
+    return userver::formats::json::ValueBuilder{orgs}.ExtractValue();
+}
+
+}  // namespace pawspective::handlers
