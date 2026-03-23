8cfcded0c92af98f8bdf19f179d2f64d6c6016b0
LeraShil <valeriya2007@icloud.com>
2026-03-23 10:58:14 +0000

    refactor: change animal_filters.cpp

--- src/models/src/animal_filters.cpp
+++ src/models/src/animal_filters.cpp
@@ -12,29 +12,47 @@ AnimalFilters AnimalFilters::from_dto(const dto::AnimalFilterDTO& dto) {
     model.colors = dto.colors.value_or(std::vector<AnimalColor>{});
     model.good_withs = dto.good_withs.value_or(std::vector<GoodWith>{});
 
-    model.min_age = dto.age_lte.value_or(0);
-    model.max_age = dto.age_gte.value_or(100);
+    model.min_age = dto.age_gte.value_or(0);
+    model.max_age = dto.age_lte.value_or(100);
 
     return model;
 }
 
 dto::AnimalFilterDTO AnimalFilters::to_dto(const AnimalFilters& model) {
     dto::AnimalFilterDTO dto;
-    auto filter_unspecified = [](auto vec) {
-        using T = typename decltype(vec)::value_type;
+    auto filter_unspecified = [](auto& vec) {
+        using T = typename std::decay_t<decltype(vec)>::value_type;
         vec.erase(std::remove(vec.begin(), vec.end(), T::kUnspecified), vec.end());
-        return vec;
     };
     dto.breeds = model.breed_ids;
-    dto.animal_types = filter_unspecified(model.animal_types);
-    dto.sizes = filter_unspecified(model.sizes);
-    dto.genders = filter_unspecified(model.genders);
-    dto.care_levels = filter_unspecified(model.care_levels);
-    dto.colors = filter_unspecified(model.colors);
-    dto.good_withs = filter_unspecified(model.good_withs);
+    dto.animal_types = model.animal_types;
+    dto.sizes = model.sizes;
+    dto.genders = model.genders;
+    dto.care_levels = model.care_levels;
+    dto.colors = model.colors;
+    dto.good_withs = model.good_withs;
 
-    dto.age_lte = model.min_age;
-    dto.age_gte = model.max_age;
+    if (dto.animal_types.has_value()) {
+        filter_unspecified(dto.animal_types.value());
+    }
+    if (dto.sizes.has_value()) {
+        filter_unspecified(dto.sizes.value());
+    }
+    if (dto.genders.has_value()) {
+        filter_unspecified(dto.genders.value());
+    }
+    if (dto.care_levels.has_value()) {
+        filter_unspecified(dto.care_levels.value());
+    }
+    if (dto.colors.has_value()) {
+        filter_unspecified(dto.colors.value());
+    }
+    if (dto.good_withs.has_value()) {
+        filter_unspecified(dto.good_withs.value());
+    }
+
+    dto.age_gte = model.min_age;
+    dto.age_lte = model.max_age;
 
     return dto;
 }
