272485b325a46fb7854388bc8889ab93ed54fb2e
LeraShil <valeriya2007@icloud.com>
2026-03-24 00:22:04 +0000

    feat: implement animal filters handler

---
+++ src/handlers/include/animal_filters_handler.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <userver/server/handlers/http_handler_json_base.hpp>
+#include "animal_service_component.hpp"
+
+namespace pawspective::handlers {
+
+class AnimalFiltersHandler final : public userver::server::handlers::HttpHandlerJsonBase {
+public:
+    constexpr static std::string_view kName = "handler-animal-filters";
+
+    AnimalFiltersHandler(
+        const userver::components::ComponentConfig& config,
+        const userver::components::ComponentContext& component_context,
+        bool is_monitor = false
+    );
+
+    userver::formats::json::Value HandleRequestJsonThrow(
+        const userver::server::http::HttpRequest& request,
+        const userver::formats::json::Value& request_json,
+        userver::server::request::RequestContext& context
+    ) const override;
+
+private:
+    const components::AnimalServiceComponent& animal_service_;
+};
+
+}  // namespace pawspective::handlers
