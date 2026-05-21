d2b75b49d7cc84782629890dcc23f9fb8ffb12eb
Egor Maximov <maksimov.egor07@gmail.com>
2026-05-21 12:14:28 +0300

    feat: add review create handler

--- src/main.cpp
+++ src/main.cpp
@@ -46,6 +46,7 @@
 #include "post_repository_component.hpp"
 #include "post_service_component.hpp"
 #include "posts_get_handler.hpp"
+#include "review_create_handler.hpp"
 #include "review_repository_component.hpp"
 #include "review_service_component.hpp"
 #include "session_cleaner_component.hpp"
@@ -104,6 +105,7 @@ int main(int argc, char* argv[]) {
             .Append<pawspective::handlers::AnimalGetHandler>()
             .Append<pawspective::handlers::AnimalListHandler>()
             .Append<pawspective::handlers::AnimalFiltersHandler>()
+            .Append<pawspective::handlers::ReviewCreateHandler>()
             .Append<pawspective::handlers::UserDeleteHandler>()
             .Append<pawspective::handlers::OrganizationDeleteHandler>()
             .Append<pawspective::handlers::AnimalDeleteHandler>()
